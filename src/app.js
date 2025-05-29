85c614ccba3457319681245d272fe091f480b3fc
mdarsalan007 <arsalansiddiquie007@gmail.com>
2025-05-29 13:33:37 +0530

    api level data sanitization

--- src/app.js
+++ src/app.js
@@ -23,6 +23,63 @@ app.post("/signup",async (req,res)=>{
     }
 })
 
+app.get("/feed", async (req,res)=>{
+    try{
+        const users = await User.find({});
+        res.send(users);
+    }
+    catch{
+        res.status(400).send("something wnet wrong!")
+    }
+})
+
+
+app.get("/user",async (req,res)=>{
+    try{
+        const user = await User.findOne({emailId:"arsalan@dummy.com"});
+        res.send(user);
+    }
+    catch (err){
+        res.status(400).send("something went wrong!");
+    }
+})
+
+app.delete("/user",async (req,res)=>{
+    try{
+        const id=req.body.id;
+        await User.findByIdAndDelete(id);
+        res.send("deleted user of matching id successfully")
+    }
+    catch{
+        res.status(400).send("something went wrong while deleting the user")
+    }
+   
+})
+
+app.patch("/user/:id",async (req,res)=>{
+        const id = req.params?.id;
+        const data = req.body;
+
+    try {
+        const allowedUpadtes = ["photoUrl","about","skills"];
+        const isAllowed = Object.keys(data).every((k)=>
+            allowedUpadtes.includes(k)
+        )
+        if(!isAllowed){
+            throw new Error("Update not allowed");
+        }
+        await User.findByIdAndUpdate(id,data,{runValidators:true,});
+        res.send("data updated successfully")
+    }
+    catch(err){
+        res.status(400).send("Update failed"+err.message)
+    }
+
+    
+})
+
+
+
 connectDB()
     .then(()=>{
         console.log("connected to the database successfully");
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,78 @@
 const mongoose = require("mongoose");
+const imageExtensionRegex = /\.(jpg|jpeg|png)$/i;
 
-const userSchema =new mongoose.Schema({
-    firstName:{
-        type:String
-    },
-    lastName:{
-        type:String
-    },
-    emailId:{
-        type:String
-    },
-    password:{
-        type:String
-    },
-    age:{
-        type:Number
-    },
-    gender:{
-        type:String
-    }
 
-})
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength:26
+    },
+    lastName: {
+      type: String,
+      minLength: 1,
+      maxLength:26
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      maxLength:50
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength:8,
+      maxLength:30
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max:150
+    },
+    gender: {
+      type: String,
+      // how to add a custom validation function (but by default it will only be called in signup not in path[means updating ] for that u have to pass an opton runValidators in patch)
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1k4G5-S8HdbqcAokpwZBfwUQKFhodPBt1_8Xv78Q0Pm805bBRyJVfCaI&s",
+        maxlength: 500,
+        validate: function(v) {
+            if(!imageExtensionRegex.test(v)){
+                throw new Error("only jpg, png and jpeg file type allowed")
+            };
+        }
 
-module.exports = mongoose.model("User",userSchema);
+
+    },
+    about: {
+      type: String,
+      default: "This is default about for a user",
+      maxLength:200
+    },
+    skills: {
+      type: [String],
+      validate(v){
+        if(v.length > 15){
+            throw new Error("Maximum you can add 15 skills.")
+        }
+      }
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
